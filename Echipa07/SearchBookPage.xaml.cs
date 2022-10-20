68937714e8e11b79c12143b5d7642a09f4b787ce
Mireag Madalina <mireagmada@gmail.com>
2022-10-20 15:06:42 +0300

    menu item context (pe listView) add book to favourites

--- Echipa07/SearchBookPage.xaml.cs
+++ Echipa07/SearchBookPage.xaml.cs
@@ -1,13 +1,25 @@
+using Microsoft.Maui.Controls;
+
 namespace Echipa07;
 
 public partial class SearchBookPage : ContentPage
 {
     SearchBookViewModel viewModel1 { get; set; }
+    List<Book> favouriteBooks = new List<Book>();
     public SearchBookPage()
 	{
 		InitializeComponent();
         viewModel1 = new SearchBookViewModel();
         BindingContext = viewModel1;
+
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        MenuItem menuItem = sender as MenuItem;
+        var contextItem = menuItem.BindingContext;
+        favouriteBooks.Add(contextItem as Book);
+        // ma gandeam aici sa salvam cartea in BD (sa avem cartile favorite stocate) si sa facem o pagina unde le afisam din BD pe cele favorite
 
     }
 }
