438c11ca6e8497393d018a5dc181febff0d68755
cmihaila99 <cati.mihaila1999@gmail.com>
2022-11-05 00:53:49 +0200

    pagina contact us + display alert la click pe buton

--- Echipa07/ContactPage.xaml.cs
+++ Echipa07/ContactPage.xaml.cs
@@ -6,4 +6,12 @@ public partial class ContactPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void buttonContact_Clicked(object sender, EventArgs e)
+	{
+        await DisplayAlert("Message sent", "Thank you for your message! Have a nice day! :D", "OK");
+		editorEmail.Text = "";
+		entryName.Text = "";
+		editorMessage.Text = "";
+    }
 }
